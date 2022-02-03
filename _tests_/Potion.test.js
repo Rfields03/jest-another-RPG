ab6ddb775334f92eb705e0e34e3532580c6b7e11
Robert Field <rfields03@gmail.com>
2022-02-03 13:55:50 -0500

    update files

--- _tests_/Potion.test.js
+++ _tests_/Potion.test.js
@@ -1,3 +1,12 @@
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+  const potion = new Potion('health');
+
+  expect(potion.name).toBe('health');
+  expect(potion.value).toEqual(expect.any(Number));
+});
+
 function Potion(name) {
   this.types = ['strength', 'agility', 'health'];
   this.name = name || this.types[Math.floor(Math.random() * this.types.length)];
